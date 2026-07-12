550921acf7d29d8baaea1c4455c2577ddc1ff08d
Diya H.Prajapati <diyap2359@gmail.com>
2026-07-12 11:08:04 +0530

    all features are done

--- backend/routes/trip.routes.js
+++ backend/routes/trip.routes.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getAllTrips,
-  getTripById,
-  createTrip,
-  updateTrip,
-  deleteTrip,
-} = require('../controllers/trip.controller');
-const { protect, authorize } = require('../middleware/auth.middleware');
+const { getAllTrips, getTripById, createTrip, updateTrip, deleteTrip } = require('../controllers/trip.controller');
+const { protect } = require('../middleware/auth.middleware');
 
 router.use(protect);
 
-router.route('/')
-  .get(getAllTrips)
-  .post(authorize('FleetManager'), createTrip);
-
-router.route('/:id')
-  .get(getTripById)
-  .put(authorize('FleetManager'), updateTrip)
-  .delete(authorize('FleetManager'), deleteTrip);
+router.route('/').get(getAllTrips).post(createTrip);
+router.route('/:id').get(getTripById).put(updateTrip).delete(deleteTrip);
 
 module.exports = router;
